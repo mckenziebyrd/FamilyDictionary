1bf35694276a8b0741312931ba2792e8fb1d1cc6
McKenzie Byrd <mckenziebyrd@me.com>
2022-08-01 20:26:19 -0500

    Add project files.

---
+++ FamilyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FamilyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //define a dictionary
+
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+            myFamily.Add("sister2", new Dictionary<string, string>() { { "name", "Becky" }, { "age", "22" } });
+            myFamily.Add("father", new Dictionary<string, string>() { { "name", "Bob" }, { "age", "72" } });
+            myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Martha" }, { "age", "67" } });
+
+
+
+            foreach (var family in myFamily)
+                    {
+                Console.WriteLine($"{family.Value["name"]} is my {family.Key} and is {family.Value["age"]}");
+            }
+
+
+            Console.WriteLine(myFamily["sister"]["name"]);
+
+            Console.WriteLine(myFamily["sister"]);
+            Console.ReadLine();
+        }
+    }
+}
